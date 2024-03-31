5d60d5a62f09a8d36e01b05a0b63b5af9aafc8ff
Ayoub farhi <57156582+senpai1282@users.noreply.github.com>
2024-03-31 23:26:01 +0000

    hi

--- print_list.c
+++ print_list.c
@@ -1,18 +1,17 @@
 #include "monty.h"
-/**
- * print_dlistint - function that prints nodes data in a dlinkedlist
- * @h: header pointer to the dlinked list
- * Return: amount of nodes
- */
-size_t print_dlistint(stack_t *h)
-{
-	int nodes = 0;
+#include <stdio.h>
 
-	while (h)
-	{
-		printf("%d\n", h->n);
-		nodes++;
-		h = h->next;
-	}
-	return (nodes);
+/**
+ * pint - Prints the value at the top of the stack, followed by a new line.
+ * @stack: Double pointer to the top of the stack.
+ * @line_number: The current line number in the file.
+ */
+void pint(stack_t **stack, unsigned int line_number)
+{
+    if (*stack == NULL)
+    {
+        fprintf(stderr, "L%d: can't pint, stack empty\n", line_number);
+        exit(EXIT_FAILURE);
+    }
+    printf("%d\n", (*stack)->n);
 }
